6fc5dc35c5b8e3abf94fa008f947af9863f253f2
Florian Fromm <ffromm@Mac.home>
2025-04-20 00:51:13 +0200

    refactor: replace kcl by garde

---
+++ docs/src/configuration.md
@@ -0,0 +1,10 @@
+# Configuration
+
+## Full Default Configuration
+
+```yaml
+api:
+  port: 80
+gateway:
+  port: 8150
+```
--- src/configuration/api.rs
+++ src/configuration/api.rs
@@ -1,6 +1,12 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, serde::Deserialize, garde::Validate)]
 pub struct Configuration {
+    #[garde(custom(super::validation::is_valid_port))]
+    #[serde(default)]
     pub port: u16,
+}
+
+impl Default for Configuration {
+    fn default() -> Self {
+        Self { port: 8150 }
+    }
 }
